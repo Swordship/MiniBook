9beb39426a53797b2523a869ce6f2a59e5318e31
Monish <monish635112@gmail.com>
2025-10-13 20:33:11 +0530

    (fix) : express router import problem so it lead to crash

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 require('dotenv').config();
-
+const authRoutes = require('./routes/authRoute.js');
 const app = express();
 
 app.use(helmet());
@@ -19,5 +19,7 @@ app.get('/health', (req, res) => {
         environment: process.env.NODE_ENV
     });
 });
+
+app.use('/auth' ,authRoutes);
 
 module.exports = app;
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,10 @@
-const {express} = require('express');
-const routes = express.Router();
+const express = require('express');
+const router = express.Router();
 const {register} = require('../controllers/authController');
 const {login} = require('../controllers/authController');
 
-routes.post('/register', register);
+router.post('/register', register);
 
-routes.post('/login', login);
+router.post('/login', login);
 
-module.exports = routes;
+module.exports = router;
