925fe64f30824c01764106cd251e5d40ccfc69b0
Monish <monish635112@gmail.com>
2025-10-13 20:25:13 +0530

    user login feature missed file

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,7 +1,10 @@
 const {express} = require('express');
 const routes = express.Router();
 const {register} = require('../controllers/authController');
+const {login} = require('../controllers/authController');
 
 routes.post('/register', register);
+
+routes.post('/login', login);
 
 module.exports = routes;
